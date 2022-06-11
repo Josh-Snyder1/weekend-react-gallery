1e59faeaa37038d8b5f6e916bc762e5cbf6ff6dc
Josh Snyder <joshrsnyder@gmail.com>
2022-06-10 21:19:07 -0500

    got update button working Still needs getGallery

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -6,20 +6,34 @@ const pool = require('../modules/pool.js');
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
-router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
+router.put('/:id', (req, res) => {
+    //update database
+    console.log('in router.put',req.body.likes)
+    const sqlText = `
+        UPDATE "images"
+        SET "likes" = $2
+        WHERE id = $1`
+    //set variables for params
+    const deleteId = req.params.id;
+    const updatedLikes = req.body.likes;
+    console.log('inrouter.put',deleteId,updatedLikes);
+    // const isPurchased = req.body.isPurchased;
+    //assign variables to params
+    const sqlParams = [deleteId,updatedLikes];
+    //send sql query to database
+    pool.query(sqlText,sqlParams)
+    .then((result) => {
+        res.send(result.row);
+        console.log('in items.router router.put then',)
+    })
+    .catch((error) => {
+        console.log('error making database update', error);
+    });
+});
 
 // GET Route
 router.get('/', (req, res) => {
-    const sqlText = `SELECT * FROM images ORDER BY likes ASC;`;
+    const sqlText = `SELECT * FROM images ORDER BY likes DESC;`;
     pool.query(sqlText)
         .then((result) => {
 
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,4 +1,5 @@
 // import MuiImage from '../MuiImage/MuiImage'
+import updateLikes from '../updateLikes/updateLikes'
 
 function GalleryItem ({image}) {
 
@@ -6,7 +7,7 @@ function GalleryItem ({image}) {
     return (
         <div className="gallery-item">
             <img src={image.path} className="gallery-image"/>
-            <button className="love-it-btn">Love It!</button>
+            <button className="love-it-btn" onClick={() => updateLikes(image.id,image.likes)} >Love It!</button>
 
             <p>{image.likes} People Love This</p>
             {/* <MuiImage 
